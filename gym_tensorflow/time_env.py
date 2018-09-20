b57bb6fe11a0d39e27b2beb77bdcab8ff2a02187
Jongwook Choi <wookayin@gmail.com>
2018-09-20 04:17:29 -0400

    Update speed test code (time_env.py)

--- gym_tensorflow/time_env.py
+++ gym_tensorflow/time_env.py
@@ -21,38 +21,64 @@ import tensorflow as tf
 import gym_tensorflow
 from gym_tensorflow.wrappers import AutoResetWrapper
 
-def main(num_actors=128, num_threads=16):
+def main(num_actors, num_threads, game_id='pong',
+         use_tfenv=True, render=False):
+    '''Speed test.
+
+    Using 16 actors * 1 threads, it should be as high as 10000 step/s.
+    Using 128 actors * 16 threads (full stress test), it should be as high as 30000 steps/s
+      under a 40-thread (20-CPU * 2 HT) CPU machine.
+    '''
     counter = tf.Variable(0, tf.int64)
 
     def make_env():
-        env = AutoResetWrapper(gym_tensorflow.atari.AtariEnv('pong', num_actors))
-        tf_rew, tf_done=env.step(tf.zeros((num_actors,), tf.int32))
-        with tf.control_dependencies([tf_rew]):
-            return tf.assign_add(counter, num_actors, use_locking=True)
+        env = AutoResetWrapper(gym_tensorflow.atari.AtariEnv(game_id, num_actors))
+        tf_rew, tf_done = env.step(tf.zeros((num_actors,), tf.int32))
+        render_op = env.env.render() if render else tf.no_op()
+
+        reward_op = tf_rew
+        counter_op = tf.assign_add(counter, num_actors, use_locking=True)
+
+        def _run(sess):
+            sess.run([reward_op, counter_op, render_op])
+        return _run
 
     step_op = [make_env() for _ in range(num_threads)]
 
-    def thread_f(sess, op):
+    def thread_f(sess, e):
         while True:
-            sess.run(op)
+            e(sess)
+            time.sleep(0)
 
     with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
         sess.run(tf.global_variables_initializer())
 
-        threads = [threading.Thread(target=thread_f, args=(sess, op)) for op in step_op]
+        threads = [threading.Thread(target=thread_f, args=(sess, e)) for e in step_op]
         for t in threads:
             t.setDaemon(True)
             t._state = 0
             t.start()
 
+        print("main thread start")
         tstart = time.time()
         num_steps = 0
         while True:
-            diff = sess.run(counter)-num_steps
-            print('Rate: {:.0f} steps/s'.format(diff / (time.time() - tstart)))
+            diff = sess.run(counter) - num_steps
+            time_str = time.strftime('%Y%m%d-%H%M%S')
+            print('{}  Rate: {:.0f} steps/s'.format(time_str, diff / (time.time() - tstart)), flush=True)
             tstart = time.time()
             num_steps += diff
             time.sleep(5)
 
+
 if __name__ == "__main__":
-    main()
+    import argparse
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--num-actors', default=128, type=int, help='Number of actors per thread.')
+    parser.add_argument('--num-threads', default=16, type=int, help='Number of threads to run.')
+    parser.add_argument('--render', action='store_true', help='If set, fetch render op as well (slower)')
+    args = parser.parse_args()
+
+    main(num_actors=args.num_actors,
+         num_threads=args.num_threads,
+         render=args.render)
